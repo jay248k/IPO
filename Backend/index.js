4365e99d1fdf9aec80cf2370fc3690d779da86e5
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2026-01-26 06:12:36 +0530

    solve error of exit

--- Backend/index.js
+++ Backend/index.js
@@ -5,18 +5,19 @@ import { IPORouter } from './Routes/IPO.route.js';
 import { CustomerRouter } from './Routes/Customer.route.js';
 import cors from 'cors';
 const app=express();
-// app.use(cors({
-//     origin: 'https://ipo-ubmb.onrender.com', 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }));
-app.use(cors())
+app.use(cors({
+    origin: 'https://ipoinfo.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+// app.use(cors())
 app.use(express.json());
 app.use('/api/admin',AdminRouter)
 app.use('/api/ipo',IPORouter)
 app.use('/api/customer',CustomerRouter)
 
-const PORT= process.env.URL||8080;
-app.listen(PORT,()=>{
-    console.log(`${PORT}`)
-})
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
